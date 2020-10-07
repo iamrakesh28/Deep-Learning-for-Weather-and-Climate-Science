f52534ee8979c03ccc591784d9f0a7ad59fa10c1
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2020-10-07 18:30:45 +0530

    subplots added

--- lstm-keras/conv-lstm-one.py
+++ lstm-keras/conv-lstm-one.py
@@ -7,11 +7,13 @@ import matplotlib.pyplot as plt
 def load_data(filename):
     data = np.load(filename)
     #print (data[:100, :10, :, :].shape)
-    X = data[:100, :10, :, :].reshape(100, 10, 64, 64, 1)
-    Y = data[:100, 10:20, :, :].reshape(100, 10, 64, 64, 1)
+    print(data.shape)
+    X = data[:, :10, :, :].reshape(1000, 10, 64, 64, 1)
+    Y = data[:, 10:20, :, :].reshape(1000, 10, 64, 64, 1)
     return (X, Y)
 
 if __name__ == "__main__":
+    
     model = keras.Sequential(
         [
             keras.Input(
@@ -28,13 +30,31 @@ if __name__ == "__main__":
 
     model.compile(loss="binary_crossentropy", optimizer="adadelta")
     model.summary()
+    
     X, Y = load_data("../train1000.npy")
     print(X.shape, Y.shape)
-    model.fit(X, Y, batch_size=10, epochs=1, verbose=2, validation_split=0.1)
+    model.fit(X[:200], Y[:200], batch_size=10, epochs=5, verbose=2, validation_split=0.1)
 
     # predict
-    result = model.predict(X[:1, :, :, :]).reshape(10, 64, 64)
-    print(result.shape)
-    for i in range(10):
-        plt.imshow(result[0], cmap="gray")
-        plt.show()
+    pred = model.predict(X[300:301, :, :, :]).reshape(10, 64, 64)
+    actual = Y[300:301, :, :, :].reshape(10, 64, 64)
+
+    fig, ax = plt.subplots(2, 10)
+    i = 0
+    for row in ax:
+        for col in range(len(row)):
+            if i == 1:
+                row[col].imshow(pred[len(row) - col - 1], cmap="gray")
+            else:
+                row[col].imshow(actual[col], cmap="gray")
+        i += 1
+        
+    plt.show()
+    #print(result.shape)
+    '''
+    fig, ax = plt.subplots(2, 10)
+    for row in ax:
+        for col in range(len(row)):
+            row[col].imshow(X[15][col], cmap="gray")
+    plt.show()
+    '''
